25147817dde3ec52e74aa994e6e31a747b73c4fd
dizhipeng.dzp <dizhipeng.dzp@alibaba-inc.com>
2025-04-29 15:26:54 +0800

    bug fix

--- diffsynth_engine/models/base.py
+++ diffsynth_engine/models/base.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from typing import Dict, Union
 from diffsynth_engine.utils.loader import load_file
+from typing import List
 
 from diffsynth_engine.models.basic.lora import LoRALinear, LoRAConv2d
 from diffsynth_engine.models.utils import no_init_weights
