623e32b393cfcd4dc11bc5ac1845de915230a0d5
dizhipeng.dzp <dizhipeng.dzp@alibaba-inc.com>
2025-04-30 16:04:49 +0800

    bug fix

--- tests/test_models/test_siglip.py
+++ tests/test_models/test_siglip.py
@@ -1,12 +1,13 @@
 import torch
 from diffsynth_engine.models.components.siglip import SiglipImageEncoder
+from diffsynth_engine import fetch_model
 from tests.common.test_case import ImageTestCase
 
 
 class TestSiglipImageEncoder(ImageTestCase):
     @classmethod
     def setUpClass(cls):
-        model_path = "muse/google-siglip-so400m-patch14-384"
+        model_path = fetch_model("muse/google-siglip-so400m-patch14-384", path='model.safetensors')
         cls.image_encoder = SiglipImageEncoder.from_pretrained(model_path, device="cuda:0", dtype=torch.bfloat16)
 
     def test_siglip_image_encoder(self):
