bd7982b42fdc8d0f39e8b7972397bbe15d9687a3
chuzihao.czh <chuzihao.czh@alibaba-inc.com>
2025-05-22 20:00:49 +0800

    code purify

--- tests/test_pipelines/test_flux_bfl_image.py
+++ tests/test_pipelines/test_flux_bfl_image.py
@@ -6,25 +6,26 @@ from diffsynth_engine.pipelines.flux_image import ControlType, ControlNetParams
 from diffsynth_engine.processor.canny_processor import CannyProcessor
 from diffsynth_engine.processor.depth_processor import DepthProcessor
 
-# from diffsynth_engine import fetch_model
+from diffsynth_engine import fetch_model
 from PIL import Image
 
 
-class TestFLUXBFLImage(ImageTestCase):
+class TestFLUXBFLCannyImage(ImageTestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.canny_processor = CannyProcessor("cuda:0")
+        cls.canny_model_path = fetch_model(
+            "AI-ModelScope/FLUX.1-Canny-dev", revision="master", path="flux1-canny-dev.safetensors"
+        )
 
-    def _test_canny_txt2img(self) -> None:
-        self.canny_processor = CannyProcessor("cuda:0")
-        # self.canny_model_path = fetch_model(
-        #     "AI-ModelScope/FLUX.1-Canny-dev", revision="master", path="flux1-canny-dev.safetensors"
-        # )
-        self.canny_model_path = "/mnt/muse-amd-nas/flux1-canny-dev.safetensors"
+        cls.pipe = FluxImagePipeline.from_pretrained(cls.canny_model_path, control_type=ControlType.bfl_control).eval()
 
-        self.pipe = FluxImagePipeline.from_pretrained(
-            self.canny_model_path, control_type=ControlType.bfl_control
-        ).eval()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        del cls.pipe
+        return super().tearDownClass()
+
+    def test_canny_txt2img(self) -> None:
         width, height = 1024, 1024
         image = self.get_input_image("test_image.png").resize((width, height), Image.LANCZOS)
         control_image = self.canny_processor(image)
@@ -44,17 +45,23 @@ class TestFLUXBFLImage(ImageTestCase):
         )
         self.assertImageEqualAndSaveFailed(image, "flux/flux_bfl_canny.png", threshold=0.99)
 
-    def _test_depth_txt2img(self):
-        self.depth_processor = DepthProcessor("cuda:0")
-        # self.depth_model_path = fetch_model(
-        #     "AI-ModelScope/FLUX.1-Depth-dev", revision="master", path="flux1-depth-dev.safetensors"
-        # )
 
-        self.depth_model_path = "/mnt/muse-amd-nas/flux1-depth-dev.safetensors"
+class TestFLUXBFLDepthImage(ImageTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.depth_processor = DepthProcessor("cuda:0")
+        cls.depth_model_path = fetch_model(
+            "AI-ModelScope/FLUX.1-Depth-dev", revision="master", path="flux1-depth-dev.safetensors"
+        )
 
-        self.pipe = FluxImagePipeline.from_pretrained(
-            self.depth_model_path, control_type=ControlType.bfl_control
-        ).eval()
+        cls.pipe = FluxImagePipeline.from_pretrained(cls.depth_model_path, control_type=ControlType.bfl_control).eval()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        del cls.pipe
+        return super().tearDownClass()
+
+    def test_depth_txt2img(self):
         width, height = 1024, 1024
         image = self.get_input_image("robot.png").resize((width, height), Image.LANCZOS)
         control_image = self.depth_processor(image)
@@ -74,14 +81,21 @@ class TestFLUXBFLImage(ImageTestCase):
         )
         self.assertImageEqualAndSaveFailed(image, "flux/flux_bfl_depth.png", threshold=0.99)
 
+
+class TestFLUXBFLFillImage(ImageTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.fill_model_path = fetch_model(
+            "AI-ModelScope/FLUX.1-Fill-dev", revision="master", path="flux1-fill-dev.safetensors"
+        )
+        cls.pipe = FluxImagePipeline.from_pretrained(cls.fill_model_path, control_type=ControlType.bfl_fill).eval()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        del cls.pipe
+        return super().tearDownClass()
+
     def test_fill_txt2img(self):
-        # self.fill_model_path = fetch_model(
-        #     "AI-ModelScope/FLUX.1-Fill-dev", revision="master", path="flux1-fill-dev.safetensors"
-        # )
-
-        self.fill_model_path = "/mnt/muse-amd-nas/flux1-fill-dev.safetensors"
-
-        self.pipe = FluxImagePipeline.from_pretrained(self.fill_model_path, control_type=ControlType.bfl_fill).eval()
         width, height = 1232, 1632
         image = self.get_input_image("cup.png").resize((width, height), Image.LANCZOS)
         mask = self.get_input_image("cup_mask.png").resize((width, height), Image.LANCZOS)
