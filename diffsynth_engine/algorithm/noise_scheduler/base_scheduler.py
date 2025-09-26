4a4c8ee490b293862adba4a1088996fd8c17a8d5
yueyang.hyy <yueyang.hyy@alibaba-inc.com>
2025-09-26 17:31:49 +0800

    del attr not in __init__ after restore scheduler

--- diffsynth_engine/algorithm/noise_scheduler/base_scheduler.py
+++ diffsynth_engine/algorithm/noise_scheduler/base_scheduler.py
@@ -10,19 +10,22 @@ class BaseScheduler:
         self._initial_params = {}
 
     def store_initial_config(self):
-        for attr_name in dir(self):
-            if not attr_name.startswith('_') and not callable(getattr(self, attr_name)):
-                self._initial_params[attr_name] = getattr(self, attr_name)
+        self._initial_params = {attr_name: attr_value for attr_name, attr_value in vars(self).items()}
 
     def update_scheduler_config(self, config_dict):
         for param_name, new_value in config_dict.items():
-            if hasattr(self, param_name) and getattr(self, param_name) != new_value:
+            if hasattr(self, param_name):
                 setattr(self, param_name, new_value)
 
     def restore_scheduler_config(self):
+        current_attrs = set(vars(self).keys())
+        initial_attrs = set(self._initial_params.keys())
+
         for param_name, initial_value in self._initial_params.items():
-            if hasattr(self, param_name):
-                setattr(self, param_name, initial_value)
+            setattr(self, param_name, initial_value)
+
+        for attr_name in current_attrs - initial_attrs:
+            delattr(self, attr_name)
 
     def schedule(self, num_inference_steps: int):
         raise NotImplementedError()
