07f6dc4395135e59e012c94f61ce70ad0b698576
yueyang.hyy <yueyang.hyy@alibaba-inc.com>
2025-09-28 17:47:07 +0800

    fix tab

--- diffsynth_engine/algorithm/noise_scheduler/base_scheduler.py
+++ diffsynth_engine/algorithm/noise_scheduler/base_scheduler.py
@@ -9,11 +9,12 @@ class BaseScheduler:
     def __init__(self):
         self._stored_config = {}
 
-    def __post_init__(self):
-        self.store_config()
-
     def store_config(self):
-        self._stored_config = {config_name: config_value for config_name, config_value in vars(self).items() if not config_name.startswith('_')}
+        self._stored_config = {
+            config_name: config_value
+            for config_name, config_value in vars(self).items()
+            if not config_name.startswith("_")
+        }
 
     def update_config(self, config_dict):
         for config_name, new_value in config_dict.items():
@@ -21,15 +22,8 @@ class BaseScheduler:
                 setattr(self, config_name, new_value)
 
     def restore_config(self):
-        current_configs = set(vars(self).keys())
-        initial_configs = set(self._stored_config.keys())
-
-        for config_name, initial_value in self._stored_config.items():
-            setattr(self, config_name, initial_value)
-
-        for config_name in current_configs - initial_configs:
-            if not config_name.startswith('_'):
-                delattr(self, config_name)
+        for config_name, config_value in self._stored_config.items():
+            setattr(self, config_name, config_value)
 
     def schedule(self, num_inference_steps: int):
         raise NotImplementedError()
